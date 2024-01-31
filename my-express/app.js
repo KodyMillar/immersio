57c32039a4c96d5c8bed72c07a7836f284698630
Baegeon Kim <baegeonkim@BaegeoncBookPro.hitronhub.home>
2024-01-30 20:36:46 -0800

    modified the app.js and add db.js for MongoDB Atlas Setup

--- my-express/app.js
+++ my-express/app.js
@@ -1,25 +1,60 @@
 require('dotenv').config()
 
-import express, { json } from 'express';
-import { connect, connection } from 'mongoose';
-import cors from 'cors';
-import helmet from 'helmet';
-
-const app = express();
-const PORT = process.env.PORT || 8000;
+const express = require('express')
+const { connectToDb, getDb} = require('./db')
+// import cors from 'cors';
+// import helmet from 'helmet';
+const app = express()
+app.use(express.json());
+const PORT = 3000;
 
 // MongoDB Connection
-connect(process.env.DATABASE_URL);
-const db = connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to DB'));
 
-// Middleware
-app.use(cors());
-app.use(helmet());
-app.use(json());
+let db
+
+connectToDb((err)=>{
+    if(!err) {
+        app.listen(PORT, () => {
+          console.log(`Server started at http://localhost:${PORT} 🚀`)
+        })
+        db = getDb()
+    }
+})
+
+// // Middleware
+// app.use(cors());
+// app.use(helmet());
+// app.use(json());
 
 // Routes Here
+
+app.get('/info', (req, res) => {
+  let info = []
+  db.collection('info')
+      .find()
+      .sort( { user_name: 1})
+      .forEach( a => info.push(a))
+      .then(() => {
+          res.status(200).json(info)
+      })
+      .catch(() => {
+          res.status(500).json({error: 'Could not find the documents'})
+      })
+})
+
+app.post('/info', (req, res) => {
+  const info = req.body
+
+  db.collection('info')
+      .insertOne(info)
+      .then(result => {
+          res.status(201).json(result)
+      })
+      .catch(err => {
+          res.status(500).json({err: 'Could not add a user_info'})
+      })
+})
+
 
 
 // Error Handling Middleware
@@ -27,6 +62,3 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
-
-// Start the server
-app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT} 🚀`));

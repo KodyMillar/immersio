512d104af2a253b09466f39247c3f087e2f53cf3
tommy <tommy.nguyen.vanc@gmail.com>
2024-02-02 10:43:27 -0800

    Mongoose integration

--- my-express/routes/info.js
+++ my-express/routes/info.js
@@ -12,6 +12,11 @@ router.get('/', async (req, res) => {
     }
 })
 
+// Get one activity data
+router.get('/:id', getActivity, (req, res) => {
+    res.json(res.activity)
+})
+
 // Create a new activity
 router.post('/', async (req, res) => {
     try {
@@ -35,6 +40,35 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message })
     } 
 })
+
+// Delete an activity
+router.delete('/:id', async(req, res) => {
+    try {
+        const result = await Activity.findByIdAndDelete(req.params.id)[[]]
+        if (res.activity == null) {
+            return res.status(404).json({ message: 'Cannot find activity' })
+        }
+        res.json({ message: 'Deleted activity' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+async function getActivity(req, res, next) {
+    let activity
+    try {
+        activity = await Activity.findById(req.params.id)
+        if (activity == null) {
+            return res.status(404).json({ message: 'Cannot find activity' })
+        }
+    
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    res.activity = activity
+    next();
+}
 
 
 module.exports = router

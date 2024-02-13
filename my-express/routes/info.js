b20b00eebf766aed8aecd9a748158110fbcafb81
haniehsadgh <haniehgh7689@gmail.com>
2024-02-12 18:33:12 -0800

    added PUT method and get Activities by ID

--- my-express/routes/info.js
+++ my-express/routes/info.js
@@ -17,6 +17,11 @@ router.get('/:id', getActivity, (req, res) => {
     res.json(res.activity)
 })
 
+// Get activities by userid
+router.get('/user/:userId', getActivityByUserId ,async (req, res) => {
+    res.json(res.activity)
+})
+
 // Create a new activity
 router.post('/', async (req, res) => {
     try {
@@ -40,6 +45,38 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message })
     } 
 })
+
+// Update an activity
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedActivity = await Activity.findByIdAndUpdate(
+            req.params.id,
+            {
+                userId: req.body.userId,
+                activity: {
+                    timestamp: req.body.activity.timestamp,
+                    itemType: req.body.activity.itemType,
+                    itemId: req.body.activity.itemId,
+                    courseId: req.body.activity.courseId,
+                    lessonId: req.body.activity.lessonId,
+                    activityDetails: {
+                        activityType: req.body.activity.activityDetails.activityType,
+                        activityResponse: req.body.activity.activityDetails.activityResponse
+                    }
+                }
+            },
+            { new: true } // Return the updated document
+        );
+
+        if (!updatedActivity) {
+            return res.status(404).json({ message: 'Cannot find activity to update' });
+        }
+
+        res.json(updatedActivity);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 // Delete an activity
 router.delete('/:id', async(req, res) => {
@@ -68,6 +105,21 @@ async function getActivity(req, res, next) {
     }
     res.activity = activity
     next();
+}
+
+async function getActivityByUserId(req, res, next) {
+    let activities
+    try {
+        activities = await Activity.find({'userId': req.params.userId});
+        if (activities == null) {
+            return res.status(404).json({ message: 'Cannot find activity' })
+        }
+    
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    res.activity = activities
+    next(); 
 }
 
 

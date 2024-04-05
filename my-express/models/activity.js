691c65695c4f7892bd8221a8f98f59732b6250bf
Baegeon Kim <baegeonkim@BaegeoncBookPro.hitronhub.home>
2024-04-05 10:44:46 -0700

    resolved the confilct with main branch

--- my-express/models/activity.js
+++ my-express/models/activity.js
@@ -9,7 +9,7 @@ const detailsSchema = new mongoose.Schema({
     },
     activityType: {
         type: String,
-        //enum: process.env.ACTIVITY_TYPE_ENUM.split(","),
+        enum: process.env.ACTIVITY_TYPE_ENUM.split(","),
         required: true
     },
     timeSpent: {
@@ -48,7 +48,7 @@ const activitySchema = new mongoose.Schema({
         },
         itemType: {
             type: String,
-            //enum: process.env.ITEM_TYPE_ENUM.split(","),
+            enum: process.env.ACTIVITY_TYPE_ENUMACTIVITY_TYPE_ENUM.split(","),
             required: true
         },
         details: [detailsSchema]
